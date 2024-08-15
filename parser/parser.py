0bfae361f0d3f732d99317ce647ffb2ed4b8efe1
gravelane <gravelanedoto@gmail.com>
2024-08-15 20:07:43 +0700

    second commit

--- parser/parser.py
+++ parser/parser.py
@@ -1,37 +1,3 @@
-# from LxmlSoup import LxmlSoup
-# import requests
-
-# html = requests.get('https://novosibirsk.cian.ru/cat.php?deal_type=rent&engine_version=2&offer_type=flat&region=4897&room1=1&room2=1&room9=1&type=4').text  # получаем html код сайта
-# soup = LxmlSoup(html)  # создаём экземпляр класса LxmlSoup
-
-# links = soup.find_all('a', class_='_93444fe79c--link--VtWj6')  # получаем список ссылок и наименований
-# for i, link in enumerate(links):
-#     url = link.get("href")  # получаем ссылку товара
-#     name = link.text()  # извлекаем наименование из блока со ссылкой
-#     price = soup.find_all("span", class_="_93444fe79c--color_black_100--Ephi7 _93444fe79c--lineHeight_28px--KFXmc _93444fe79c--fontWeight_bold--BbhnX _93444fe79c--fontSize_22px--sFuaL _93444fe79c--display_block--KYb25 _93444fe79c--text--e4SBY _93444fe79c--text_letterSpacing__normal--tfToq")[i].text()  # извлекаем цену
-#     print(i)
-#     print(f"Url - {url}")
-#     print(f"Name - {name}")
-#     print(f"Price - {price}\n")
-
-# from LxmlSoup import LxmlSoup
-# import requests
-
-# html = requests.get('https://realty.ya.ru/novosibirsk/snyat/kvartira/').text  # получаем html код сайта
-# soup = LxmlSoup(html)  # создаём экземпляр класса LxmlSoup
-
-# links = soup.find_all('a', class_='Link Link_js_inited Link_size_m Link_theme_islands SerpItemLink OffersSerpItem__link OffersSerpItem__titleLink')  # получаем список ссылок и наименований
-# for i, link in enumerate(links):
-#     url = link.get("href")  # получаем ссылку товара
-#     name = link.text()  # извлекаем наименование из блока со ссылкой
-#     # price = soup.find_all("div", class_='OffersSerpItem__price PriceWithDiscount__container--QehfS')[i].text()  # извлекаем цену
-#     print(i)
-#     print(f"Url - {url}")
-#     print(f"Name - {name}")
-#     # print(f"Price - {price}\n")
-    
-
-
 
 from bs4 import BeautifulSoup
 import requests
